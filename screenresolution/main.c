bab954864f881fcddc39b95d5c6a3ffe2082a81f
John Ford <john@johnford.info>
2011-08-26 19:50:05 -0700

    rearrange code and add commands

--- screenresolution/main.c
+++ screenresolution/main.c
@@ -5,12 +5,21 @@
  * Build: clang -framework ApplicationServices main.c -o screenresolution
  * 
  * John Ford <john@johnford.info>
+ * todo:
+ *   -make printError easier for making strings
+ *   -support more than one monitor
  */
 
 #include <ApplicationServices/ApplicationServices.h>
 
-bool switchDisplayToMode (CGDirectDisplayID display, CGDisplayModeRef mode);
+void printError(char* msg);
+unsigned int setDisplayToMode (CGDirectDisplayID display, CGDisplayModeRef mode);
+unsigned int setDisplay(CGDirectDisplayID display, size_t w, size_t h, size_t d);
+unsigned int listCurrentMode(CGDirectDisplayID display);
+unsigned int listAvailableModes(CGDirectDisplayID display);
+
 size_t bitDepth(CGDisplayModeRef mode);
+
 
 int main (int argc, const char * argv[])
 {
@@ -18,57 +27,43 @@ int main (int argc, const char * argv[])
     size_t w;
     size_t h;
     size_t d;
-    CGDirectDisplayID mainDisplay;
-    
-    mainDisplay = CGMainDisplayID();
+    CGDirectDisplayID mainDisplay = CGMainDisplayID();
     
     if (argc == 1) {
-        CGDisplayModeRef currentMode;
-        currentMode = CGDisplayCopyDisplayMode(mainDisplay);
-        printf("%lux%lux%lu\n", CGDisplayModeGetWidth(currentMode), 
-                                 CGDisplayModeGetHeight(currentMode),
-                                 bitDepth(currentMode));
-        CGDisplayModeRelease(currentMode);
-    } else if (argc == 4) {
-        CFArrayRef allModes = CGDisplayCopyAllDisplayModes(mainDisplay, NULL);
-        CGDisplayModeRef newMode = NULL;
-        size_t pw; // possible width
-        size_t ph; // possible height
-        size_t pd; // possible depth
-        bool looking = true; // used to decide whether to continue looking for modes
-        w = atoi(argv[1]);
-        h = atoi(argv[2]);
-        d = atoi(argv[3]);
-        for (int i = 0; i < CFArrayGetCount(allModes) && looking ; i++) {
-            CGDisplayModeRef possibleMode = (CGDisplayModeRef) CFArrayGetValueAtIndex(allModes, i);
-            pw = CGDisplayModeGetWidth(possibleMode);
-            ph = CGDisplayModeGetHeight(possibleMode);
-            pd = bitDepth(possibleMode);
-            if ( pw == w && ph == h && pd == d ) {
-                looking = false; // Stop looking for more modes!
-                newMode = possibleMode;
-                //printf("Found matching mode(%lux%lux%lux)", pw,ph,pd);
-            }
-#ifdef DEBUG
-            else {
-                printf("DEBUG: ignoring %lux%lux%lu\n", pw,ph,pd);
-            }
-#endif
+        // By default, print out the main display's resolution
+        if (!listCurrentMode(mainDisplay)){
+            exitcode++;
         }
-            if (newMode != NULL) {
-                printf("Setting mode to %lux%lux%lu\n", pw,ph,pd);
-                switchDisplayToMode(mainDisplay,newMode);
-            } else {
-                fprintf(stderr, "Error: requested mode (%lux%lux%lu) is not available\n",w,h,d);
+    } else if (argc == 2) {
+        if (strcmp(argv[1], "get-main") == 0){
+            if (!listCurrentMode(mainDisplay)){
                 exitcode++;
             }
+        } else if (strcmp(argv[1], "list-main") == 0){
+            if (!listAvailableModes(mainDisplay)){
+                exitcode++;
+            }
+        } else {
+            printError("You must specify a command");
+            exitcode++;
+        }
+    } else if (argc == 5 && strcmp(argv[1], "set-main") == 0) {
+        w = atoi(argv[2]);
+        h = atoi(argv[3]);
+        d = atoi(argv[4]);
+        if (!setDisplay(mainDisplay, w, h, d)){
+            exitcode++;
+        }
     } else {
-        fprintf(stderr, "Usage: %s <width> <height> <depth>\n", argv[0]);
+        printError("Use it the right way!");
         exitcode++;
     }
     return exitcode;
 }
 
+void printError(char* msg){
+    fprintf(stderr, "Error: %s\n", msg);
+}
 
 size_t bitDepth(CGDisplayModeRef mode) {
     size_t depth = 0;
@@ -85,23 +80,90 @@ size_t bitDepth(CGDisplayModeRef mode) {
 	return depth;
 }
 
-bool switchDisplayToMode (CGDirectDisplayID display, CGDisplayModeRef mode) {
+unsigned int setDisplay(CGDirectDisplayID display, size_t w, size_t h, size_t d){
+    unsigned int returncode = 1;
+    CFArrayRef allModes = CGDisplayCopyAllDisplayModes(display, NULL);
+    if (allModes == NULL){
+        printError("failed trying to look up all modes for display");
+    }
+    CGDisplayModeRef newMode = NULL;
+    size_t pw; // possible width
+    size_t ph; // possible height
+    size_t pd; // possible depth
+    bool looking = true; // used to decide whether to continue looking for modes
+    for (int i = 0; i < CFArrayGetCount(allModes) && looking ; i++) {
+        CGDisplayModeRef possibleMode = (CGDisplayModeRef) CFArrayGetValueAtIndex(allModes, i);
+        pw = CGDisplayModeGetWidth(possibleMode);
+        ph = CGDisplayModeGetHeight(possibleMode);
+        pd = bitDepth(possibleMode);
+        if ( pw == w && ph == h && pd == d ) {
+            looking = false; // Stop looking for more modes!
+            newMode = possibleMode;
+        }
+    }
+    if (newMode != NULL) {
+        printf("Setting mode to %lux%lux%lu\n", pw,ph,pd);
+        setDisplayToMode(display,newMode);
+    } else {
+        fprintf(stderr, "Error: requested mode (%lux%lux%lu) is not available\n",w,h,d);
+        returncode = 0;
+    }
+    return returncode;
+}
+
+
+unsigned int setDisplayToMode (CGDirectDisplayID display, CGDisplayModeRef mode) {
+    unsigned int returncode = 1;
     CGError rc;
     CGDisplayConfigRef config;
     rc = CGBeginDisplayConfiguration(&config);
     if (rc != kCGErrorSuccess) {
-        printf("ERROR: failed CGBeginDisplayConfiguration\n");
-        return false;
+        printError("failed CGBeginDisplayConfiguration");
+        returncode = 0;
     }
-    rc = CGConfigureDisplayWithDisplayMode(config, display, mode, NULL);
-    if (rc != kCGErrorSuccess) {
-        printf("ERROR: failed CGConfigureDisplayWithDisplayMode\n");
-        return false;
+    if (returncode){
+        rc = CGConfigureDisplayWithDisplayMode(config, display, mode, NULL);
+        if (rc != kCGErrorSuccess) {
+            printError("failed CGConfigureDisplayWithDisplayMode");
+            returncode = 0;
+        }
     }
-    rc = CGCompleteDisplayConfiguration(config, kCGConfigureForSession );
-    if (rc != kCGErrorSuccess) {
-        printf("ERROR: failed CGCompleteDisplayConfiguration\n");
-        return false;
+    if (returncode){
+        rc = CGCompleteDisplayConfiguration(config, kCGConfigureForSession );
+        if (rc != kCGErrorSuccess) {
+            printf("failed CGCompleteDisplayConfiguration");
+            returncode = 0;
+        }
     }
-    return true;
+    return returncode;
+}
+unsigned int listCurrentMode(CGDirectDisplayID display){
+    unsigned int returncode = 1;
+    CGDisplayModeRef currentMode = CGDisplayCopyDisplayMode(display);
+    if (currentMode == NULL){
+        printError("unable to copy current display mode");
+        returncode = 0;
+    }
+    printf("%lux%lux%lu\n", CGDisplayModeGetWidth(currentMode), 
+           CGDisplayModeGetHeight(currentMode),
+           bitDepth(currentMode));
+    CGDisplayModeRelease(currentMode);
+    return returncode;
+}
+
+unsigned int listAvailableModes(CGDirectDisplayID display){
+    unsigned int returncode = 1;
+    CFArrayRef allModes = CGDisplayCopyAllDisplayModes(display, NULL);
+    if (allModes == NULL) {
+        returncode = 0;
+    }
+    printf("Available Modes\n");
+    for (int i = 0; i < CFArrayGetCount(allModes) && returncode; i++) {
+        CGDisplayModeRef mode = (CGDisplayModeRef) CFArrayGetValueAtIndex(allModes, i);
+        printf("  %lux%lux%lu\n",
+               CGDisplayModeGetWidth(mode),
+               CGDisplayModeGetHeight(mode),
+               bitDepth(mode));
+    }
+    return returncode;
 }
