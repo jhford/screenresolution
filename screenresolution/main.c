44c975479ae3a8d29ee092844ae24365510cfd94
John Ford <john@johnford.info>
2011-08-26 17:18:36 -0700

    Initial Commit

---
+++ screenresolution/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  screenresolution
+//
+//  Created by John Ford on 8/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
